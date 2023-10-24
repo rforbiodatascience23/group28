3e8154c4bc154dcd0f43b9f6c4900e6df426f6ea
skrendaskrenduolis <skrenda.skrenduolis@gmail.com>
2023-10-24 11:52:51 +0200

    Function 5 comments

--- R/plotaminoacids.R
+++ R/plotaminoacids.R
@@ -11,17 +11,21 @@
 #' example_aa_seq <- "FVVSQAGL"
 #' plot_amino_acids(example_aa_seq)
 plot_amino_acids <- function(amino_acid_sequence){
+  # Make list of amino acids
   amino_acid_list <- amino_acid_sequence |>
     stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
     as.character() |>
     unique()
 
+  # Count amino acids and save to data frame
   counts <- sapply(amino_acid_list, function(amino_acid) stringr::str_count(string = amino_acid_sequence, pattern =  amino_acid)) |>
     as.data.frame()
 
+  # Set dataframe column and row names
   colnames(counts) <- c("Counts")
   counts[["amino_acids"]] <- rownames(counts)
 
+  # Make bar plot
   output_plot <- counts |>
     ggplot2::ggplot(ggplot2::aes(x = amino_acids, y = Counts, fill = amino_acids)) +
     ggplot2::geom_col() +
